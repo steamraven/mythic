cbc09fe91b14eee7b66b93851edb63c783c33105
Matthew Hawn <steamraven@yahoo.com>
2024-11-16 12:13:59 -0700

    Updated requirements

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,10 @@
 sudo apt-get update
+# Prereqs to build and install
 sudo apt-get install -y --no-install-recommends gcc g++ swig git  
-sudo apt-get install -y --no-install-recommends fontconfig fonts-unifont xauth xrdp xorgxrdp xfce4 dbus-x11
+# Some nice unicode fonts
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends fontconfig fonts-unifont 
+# Allow graphics: xauth to allow forwarding the X11 window on linux and  xorg-rdp to allow Windows to remote desktop
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends xauth xrdp xorgxrdp xfce4 dbus-x11
 sudo apt-get clean 
 sudo rm -rf /var/lib/apt/lists/*
 
