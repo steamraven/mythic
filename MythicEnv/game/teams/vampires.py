d129a511b58a11283bee986b38cfda50ccd154f6
Matthew Hawn <steamraven@yahoo.com>
2024-12-15 17:42:09 +0000

    Convert vampires.py to ClonableGenerator

--- MythicEnv/game/teams/vampires.py
+++ MythicEnv/game/teams/vampires.py
@@ -5,7 +5,6 @@ Team specific data for Vampires
 from MythicEnv.game import (
     Team,
     TeamData,
-    PlayOrDoneCoroutine,
     Player,
     PlayYield,
     ActionPhase,
@@ -16,6 +15,14 @@ from MythicEnv.game import (
     board_to_action,
     action_to_board,
     MythicMischiefGame,
+)
+
+from MythicEnv.game import (
+    PlayOrDoneGenerator,
+    PlayOrDoneGeneratorImpl,
+    PlayOrDoneGeneratorImpl_Return,
+    Yield,
+    Return,
 )
 from MythicEnv import *
 from collections import defaultdict
@@ -37,7 +44,9 @@ VampireData = TeamData(
 class Vampire(Team):
     data = VampireData
 
-    def play_move_other(self, game: MythicMischiefGame, player: Player) -> PlayOrDoneCoroutine:
+    def play_move_other(
+        self, game: MythicMischiefGame, player: Player
+    ) -> PlayOrDoneGenerator:
         # Vampire: Lure
         # Move an ally or enemy 1 space
         # orthogonally or diagonally
@@ -45,139 +54,227 @@ class Vampire(Team):
         # • You can Lure players from any
         # unobstructed distance. See pg. 21.
         # • You can Lure onto a Cluttered space.
+        class PlayMoveOtherState(PlayOrDoneGeneratorImpl):
+            def send_impl(self, value: int | None) -> PlayOrDoneGeneratorImpl_Return:
+                action = value
+                other_player = game.players[player.id_ ^ 1]
+                if self.next_step():
+                    self.available_dict = dict[
+                        Coordinate, dict[Coordinate, Coordinate]
+                    ]()
 
-        available_dict = dict[Coordinate, dict[Coordinate, Coordinate]]()
-        other_player = game.players[player.id_ ^ 1]
+                    if player.move_other:
+                        for mythic in player.mythics:
+                            available = self.available_dict[mythic] = dict[
+                                Coordinate, Coordinate
+                            ]()
 
-        if player.move_other:
-            for mythic in player.mythics:
-                available = available_dict[mythic] = dict[Coordinate, Coordinate]()
+                            # Check line of sight to each opponent
+                            for opp in other_player.mythics:
+                                direction = game.line_of_sight(mythic, opp, 2)
+                                if direction:
+                                    available[opp] = direction
 
-                # Check line of sight to each opponent
-                for opp in other_player.mythics:
-                    direction = game.line_of_sight(mythic, opp, 2)
-                    if direction:
-                        available[opp] = direction
+                    if any(v for v in self.available_dict.values()):
+                        return Yield(
+                            PlayYield(
+                                player.id_,
+                                ActionPhase.USE_SKILL,
+                                1,
+                                ActionType.SELECT_SKILL,
+                                [Action.MOVE_OTHER],
+                            )
+                        )
+                    else:
+                        return Yield(
+                            PlayYield(
+                                player.id_,
+                                ActionPhase.USE_SKILL,
+                                1,
+                                ActionType.SELECT_SKILL,
+                                [],
+                            )
+                        )
+                if self.next_step():
+                    # The previous yield should return Action.MOVE_OTHER,
+                    # but because of how this co-routine is called,
+                    # the previous yield will return None,
+                    assert action is None or action == Action.MOVE_OTHER
+                    return Yield(
+                        PlayYield(
+                            player.id_,
+                            ActionPhase.MOVE_OPP,
+                            2,
+                            ActionType.SELECT_SELF,
+                            [
+                                board_to_action(*k)
+                                for k, v in self.available_dict.items()
+                                if v
+                            ],
+                        )
+                    )
+                if self.next_step():
+                    assert action is not None
+                    self.mythic = action_to_board(action)
 
+                    return Yield(
+                        PlayYield(
+                            player.id_,
+                            ActionPhase.MOVE_OPP,
+                            1,
+                            ActionType.SELECT_OPP,
+                            [
+                                board_to_action(*k)
+                                for k in self.available_dict[self.mythic].keys()
+                            ],
+                        )
+                    )
+                if self.next_step():
+                    assert action is not None
+                    opp = action_to_board(action)
 
-        # This yield should return Action.MOVE_OTHER,
-        # but because of how this co-routine is called, 
-        # the first yield will return None,
-        if any(v for v in available_dict.values()):
-            yield PlayYield(
-                player.id_,
-                ActionPhase.USE_SKILL,
-                1,
-                ActionType.SELECT_SKILL,
-                [Action.MOVE_OTHER],
-            )
-        else:
-            yield PlayYield(
-                player.id_, ActionPhase.USE_SKILL, 1, ActionType.SELECT_SKILL, []
-            )
+                    # move opponent mythic
+                    player.move_other -= 1
+                    direction = self.available_dict[self.mythic][opp]
+                    dest = (opp[0] - direction[0], opp[1] - direction[1])
+                    game.move_mythic(other_player, opp, dest)
 
-        action = yield PlayYield(
-            player.id_,
-            ActionPhase.MOVE_OPP,
-            2,
-            ActionType.SELECT_SELF,
-            [board_to_action(*k) for k, v in available_dict.items() if v],
-        )
+                return Return((False, 0))
 
-        mythic = action_to_board(action)
+        return PlayMoveOtherState()
 
-        action = yield PlayYield(
-            player.id_,
-            ActionPhase.MOVE_OPP,
-            1,
-            ActionType.SELECT_OPP,
-            [board_to_action(*k) for k in available_dict[mythic].keys()],
-        )
-
-        opp = action_to_board(action)
-
-        # move opponent mythic
-        player.move_other -= 1
-        direction = available_dict[mythic][opp]
-        dest = (opp[0] - direction[0], opp[1] - direction[1])
-        game.move_mythic(other_player, opp, dest)
-
-        return False, 0
-
-    def play_move_shelf(self, game: MythicMischiefGame, player: Player, horz: bool) -> PlayOrDoneCoroutine:
+    def play_move_shelf(
+        self, game: MythicMischiefGame, player: Player, horz: bool
+    ) -> PlayOrDoneGenerator:
         # Vampire: Conceal
         # Rotate a Bookshelf around your space.
         # • You cannot block off a section of the
         # Library completely.
         # • You cannot place a Bookshelf onto the outer
         # edge of the Library or another Bookshelf.
+        class PlayMoveShelfState(PlayOrDoneGeneratorImpl):
+            def send_impl(self, value: int | None) -> PlayOrDoneGeneratorImpl_Return:
+                action = value
+                wall_type = HORZ_WALL if horz else VERT_WALL
+                o_wall_type = VERT_WALL if horz else HORZ_WALL
+                if self.next_step():
+                    self.available_moves: dict[Wall, set[Wall]] = defaultdict(set)
+                    if player.move_shelf:
+                        for mythic in player.mythics:
+                            left = (mythic[0] - 1, mythic[1])
+                            up = (mythic[0], mythic[1] - 1)
 
-        wall_type = HORZ_WALL if horz else VERT_WALL
-        o_wall_type = VERT_WALL if horz else HORZ_WALL
+                            # don't need to test src for >=4 since it has no walls
 
-        available_moves: dict[Wall, set[Wall]] = defaultdict(set)
-        if player.move_shelf:
-            for mythic in player.mythics:
-                left = (mythic[0] - 1, mythic[1])
-                up = (mythic[0], mythic[1] - 1)
+                            game.test_wall_moves(
+                                Wall(wall_type, mythic),
+                                self.available_moves,
+                                [
+                                    Wall(o_wall_type, mythic),
+                                    Wall(o_wall_type, left if horz else up),
+                                ],
+                            )
+                            pos = up if horz else left
+                            if pos[0] >= 0 and pos[1] >= 0:
+                                game.test_wall_moves(
+                                    Wall(wall_type, pos),
+                                    self.available_moves,
+                                    [
+                                        Wall(o_wall_type, mythic),
+                                        Wall(o_wall_type, left if horz else up),
+                                    ],
+                                )
 
-                # don't need to test src for >=4 since it has no walls
-
-                game.test_wall_moves(
-                    Wall(wall_type, mythic),
-                    available_moves,
-                    [
-                        Wall(o_wall_type, mythic),
-                        Wall(o_wall_type, left if horz else up),
-                    ],
-                )
-                pos = up if horz else left
-                if pos[0] >= 0 and pos[1] >= 0:
-                    game.test_wall_moves(
-                        Wall(wall_type, pos),
-                        available_moves,
-                        [
-                            Wall(o_wall_type, mythic),
-                            Wall(o_wall_type, left if horz else up),
-                        ],
+                    # This yield should return Action.MOVE_SHELF,
+                    # but because of how this co-routine is called,
+                    # the first yield will return None,
+                    if any(
+                        src.wall_type == wall_type
+                        for src in self.available_moves.keys()
+                    ):
+                        return Yield(
+                            PlayYield(
+                                player.id_,
+                                ActionPhase.USE_SKILL,
+                                1,
+                                ActionType.SELECT_SKILL,
+                                [
+                                    (
+                                        Action.MOVE_HORZ_SHELF
+                                        if horz
+                                        else Action.MOVE_VERT_SHELF
+                                    )
+                                ],
+                            )
+                        )
+                    else:
+                        return Yield(
+                            PlayYield(
+                                player.id_,
+                                ActionPhase.USE_SKILL,
+                                1,
+                                ActionType.SELECT_SKILL,
+                                [],
+                            )
+                        )
+                if self.next_step():
+                    # This previous ;oiud should return Action.MOVE_SHELF,
+                    # but because of how this co-routine is called,
+                    # the previous yield will return None,
+                    assert action is None or action == (
+                        Action.MOVE_HORZ_SHELF if horz else Action.MOVE_VERT_SHELF
                     )
+                    return Yield(
+                        PlayYield(
+                            player.id_,
+                            (
+                                ActionPhase.MOVE_HORZ_SHELF
+                                if horz
+                                else ActionPhase.MOVE_VERT_SHELF
+                            ),
+                            2,
+                            (
+                                ActionType.SELECT_HORZ_WALL
+                                if horz
+                                else ActionType.SELECT_VERT_WALL
+                            ),
+                            [
+                                board_to_action(*src.pos)
+                                for src in self.available_moves.keys()
+                            ],
+                        )
+                    )
+                if self.next_step():
+                    assert action is not None
+                    self.src = Wall(wall_type, action_to_board(action))
 
-        # This yield should return Action.MOVE_SHELF,
-        # but because of how this co-routine is called, 
-        # the first yield will return None,
-        if any(src.wall_type == wall_type for src in available_moves.keys()):
-            yield PlayYield(
-                player.id_,
-                ActionPhase.USE_SKILL,
-                1,
-                ActionType.SELECT_SKILL,
-                [Action.MOVE_HORZ_SHELF if horz else Action.MOVE_VERT_SHELF],
-            )
-        else:
-            yield PlayYield(
-                player.id_, ActionPhase.USE_SKILL, 1, ActionType.SELECT_SKILL, []
-            )
+                    return Yield(
+                        PlayYield(
+                            player.id_,
+                            (
+                                ActionPhase.MOVE_HORZ_SHELF
+                                if horz
+                                else ActionPhase.MOVE_VERT_SHELF
+                            ),
+                            1,
+                            (
+                                ActionType.SELECT_VERT_WALL
+                                if horz
+                                else ActionType.SELECT_HORZ_WALL
+                            ),
+                            [
+                                board_to_action(*dest.pos)
+                                for dest in self.available_moves[self.src]
+                            ],
+                        )
+                    )
+                if self.next_step():
+                    assert action is not None
+                    player.move_shelf -= 1
+                    dest = Wall(o_wall_type, action_to_board(action))
 
-        action = yield PlayYield(
-            player.id_,
-            ActionPhase.MOVE_HORZ_SHELF if horz else ActionPhase.MOVE_VERT_SHELF,
-            2,
-            ActionType.SELECT_HORZ_WALL if horz else ActionType.SELECT_VERT_WALL,
-            [board_to_action(*src.pos) for src in available_moves.keys()],
-        )
-        src = Wall(wall_type, action_to_board(action))
+                    game.move_wall(self.src, dest)
 
-        action = yield PlayYield(
-            player.id_,
-            ActionPhase.MOVE_HORZ_SHELF if horz else ActionPhase.MOVE_VERT_SHELF,
-            1,
-            ActionType.SELECT_VERT_WALL if horz else ActionType.SELECT_HORZ_WALL,
-            [board_to_action(*dest.pos) for dest in available_moves[src]],
-        )
+                return Return((False, 0))
 
-        player.move_shelf -= 1
-        dest = Wall(o_wall_type, action_to_board(action))
-
-        game.move_wall(src, dest)
-
-        return False, 0
+        return PlayMoveShelfState()
